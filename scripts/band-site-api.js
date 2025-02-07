0771c0de06d3ae2540171f682612dd5806cbd500
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-07 12:14:58 -0800

    Added api

---
+++ scripts/band-site-api.js
@@ -0,0 +1,43 @@
+export class BandSiteApi {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+        this.baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com';
+    };
+
+    async getComments() {
+        const response = await axios.get(
+            this.baseUrl + "/comments?api_key=" + this.apiKey
+        );
+        const comments = response.data;
+        comments.sort(function (a, b) {
+            return b.timestamp - a.timestamp;
+        });
+        // console.log(comments);
+        return comments;
+    };
+
+    async postComments(comment) {
+        const response = await axios.post(
+            this.baseUrl + "/comments?api_key=" + this.apiKey, comment
+        );
+        const comments = response.data;
+        return comments;
+
+    }
+
+    // async deleteComment(commentId){
+    //     const response = await axios.delete(
+    //         this.baseUrl + "/comments/" + commentId + "?api_key=" + this.apiKey
+    //     );
+    //     const comments = response.data;
+    //     return comments;
+    // }
+
+    async getShow() {
+        const response = await axios.get(
+            this.baseUrl + "/showdates?api_key=" + this.apiKey
+        );
+        const shows = response.data;
+        return shows;
+    }
+}
