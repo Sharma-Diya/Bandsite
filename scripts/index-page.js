b0292faf7493259939dd4cde0e7cb73542da57d0
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-07 12:16:00 -0800

    Added async function

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,97 +1,76 @@
+import { BandSiteApi } from './band-site-api.js';
 
-
-const comments = [
-    {
-        posted: "11/02/2023",
-        name: "Victor Pinto",
-        image: "",
-        description:
-            "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    },
-    {
-        posted: "10/28/2023",
-        name: "Christina Cabrera",
-        image: "",
-
-        description:
-            "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        posted: "10/20/2023",
-        name: "Isaac Tadesse",
-        image: "",
-
-        description:
-            "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    },
-];
-// get the form from the HTML file
 const productForm = document.querySelector(".form");
-// get the product list from the HTML file
-const productList = document.querySelector(".product-list"); // <ul>
+const apiKey = "9f3187be-cf12-43b4-89d1-cfdc2094e8cb";
+const bandsiteApi = new BandSiteApi(apiKey);
+const productList = document.querySelector(".comment-list"); 
 
-console.log(comments);
-
-function renderComments() {
-
+async function getAllComments() {
+    const comments = await bandsiteApi.getComments();
     productList.innerHTML = "";
+
     for (let i = 0; i < comments.length; i++) {
         const productListItem = document.createElement("li");
-        productListItem.classList.add("product-list__item");
+        productListItem.classList.add("comment-list__item");
 
         const leftDiv = document.createElement("div");
-        leftDiv.classList.add("product-list__left");
+        leftDiv.classList.add("comment-list__image-wrapper");
         productListItem.appendChild(leftDiv);
 
         const userImage = document.createElement("div");
         userImage.src = comments[i].image;
-        userImage.classList.add("product-list__image");
+        userImage.classList.add("comment-list__image");
         leftDiv.appendChild(userImage);
 
-
         const rightDiv = document.createElement("div");
-        rightDiv.classList.add("product-list__right");
+        rightDiv.classList.add("comment-list__comment-wrapper");
         productListItem.appendChild(rightDiv);
 
         const topDiv = document.createElement("div");
-        topDiv.classList.add("product-list__top");
+        topDiv.classList.add("comment-list__top");
         rightDiv.appendChild(topDiv);
 
         const productName = document.createElement("h2");
         productName.innerText = comments[i].name;
-        productName.classList.add("product-list__name");
+        productName.classList.add("comment-list__name");
         topDiv.appendChild(productName);
 
         const productPosted = document.createElement("p");
-        productPosted.innerText = comments[i].posted;
-        productPosted.classList.add("product-list__posted");
+        const date = new Date(comments[i].timestamp);
+        productPosted.innerText = date.toLocaleDateString("en-US", {
+            month: "2-digit",
+            day: "2-digit",
+            year: "numeric"
+        });
+        productPosted.classList.add("comment-list__posted");
         topDiv.appendChild(productPosted);
 
-
         const productDescription = document.createElement("p");
-        productDescription.innerText = comments[i].description;
-        productDescription.classList.add("product-list__description");
+        productDescription.innerText = comments[i].comment;
+        productDescription.classList.add("comment-list__description");
         rightDiv.appendChild(productDescription);
 
-        
+        // const deleteButton = document.createElement("button");
+        // deleteButton.innerText = "DELETE";
+        // deleteButton.classList.add("form__button");
+        // rightDiv.appendChild(deleteButton);
 
+        // deleteButton.addEventListener("click", async () => {
+        //     await bandsiteApi.deleteComment(comments[i].id);
+        //     productListItem.remove(); // Remove from DOM
+        // });
         productList.appendChild(productListItem);
-
     }
 }
-
-productForm.addEventListener("submit", function (event) {
+productForm.addEventListener("submit", async function (event) {
     event.preventDefault();
-    const newProduct = {
-        posted: new Date().toLocaleDateString(),
-        // image: event.target.image.value,
+    const newComment = {
         name: event.target.product.value,
-        description: event.target.productDescription.value,
+        comment: event.target.productDescription.value,
     }
-    comments.unshift(newProduct);
-    console.log(newProduct);
-    renderComments();
+    await bandsiteApi.postComments(newComment);
+    await getAllComments();
+    getAllComments();
     event.target.reset();
 });
-
-renderComments();
+getAllComments();
